daecd33dc9933ca4df0648567a1d5d57ecc3eb56
brunnerklaus <kb@shpock.com>
2018-11-02 13:10:08 +0100

    Fixed js formating

--- index.js
+++ index.js
@@ -8,36 +8,36 @@ inquirer.prompt([{
   type: 'confirm',
   name: 'gitshots',
   message: 'Do you want to use gitshots?',
-  default: true
-}]).then(function (answers) {
-  if(answers.gitshots){
+  default: false
+}]).then(function(answers) {
+  if (answers.gitshots) {
 
     // additional brew packages needed to support gitshots
     config.brew.push('imagemagick', 'imagesnap')
     // ensure ~/.gitshots exists
     command('mkdir -p ~/.gitshots', __dirname, function(err, out) {
-      if(err) throw err
+      if (err) throw err
     })
     // add post-commit hook
     command('cp ./.git_template/hooks/gitshot-pc ./.git_template/hooks/post-commit', __dirname, function(err, out) {
-      if(err) throw err
+      if (err) throw err
     })
-  }else{
-    if(fs.existsSync('./.git_template/hooks/post-commit')){
+  } else {
+    if (fs.existsSync('./.git_template/hooks/post-commit')) {
       // disable post-commit (in case we are undoing the git-shots enable)
       // TODO: examine and remove/comment out the file content with the git shots bit
       command('mv ./.git_template/hooks/post-commit ./.git_template/hooks/disabled-pc', __dirname, function(err, out) {
-        if(err) throw err
+        if (err) throw err
       })
     }
   }
 
-  const installPackages = function(type){
-    console.info(emoji.get('coffee'), ' installing '+type+' packages')
-    config[type].map(function(item){
-      console.info(type+':', item)
-      command('. lib_sh/echos.sh && . lib_sh/requirers.sh && require_'+type+' ' + item, __dirname, function(err, out) {
-        if(err) console.error(emoji.get('fire'), err)
+  const installPackages = function(type) {
+    console.info(emoji.get('coffee'), ' installing ' + type + ' packages')
+    config[type].map(function(item) {
+      console.info(type + ':', item)
+      command('. lib_sh/echos.sh && . lib_sh/requirers.sh && require_' + type + ' ' + item, __dirname, function(err, out) {
+        if (err) console.error(emoji.get('fire'), err)
       })
     })
   }
