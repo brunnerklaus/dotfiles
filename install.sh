30326131014468efe9944a1ccfd14421cd076328
Klaus Brunner <bruklaus@gmail.com>
2021-10-11 20:27:19 +0200

    Update install

--- install.sh
+++ install.sh
@@ -441,6 +441,17 @@ else
   ok "Skipped installing Visual Studio Code settings"
 fi
 
+###############################################################################
+bot "📋 Install k9s configuration"
+###############################################################################
+read -r -p "Do you want to install k9s configuration now? [y|N] " response
+if [[ $response =~ (y|yes|Y) ]]; then
+  cp ./k9s/solarized_dark.yml ~/Library/Preferences/k9s/skin.yml
+  cp ./k9s/views.yml ~/Library/Preferences/k9s/views.yml
+else
+  ok "Skipped installing k9s configuration"
+fi
+
 # ###############################################################################
 # bot "📋 Configure Atom editor packages"
 # ###############################################################################
